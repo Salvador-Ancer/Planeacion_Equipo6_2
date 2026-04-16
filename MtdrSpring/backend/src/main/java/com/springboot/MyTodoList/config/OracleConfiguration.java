36df5d538cca88f031edd67649d5c06aa15df56d
API REST <a01723282@tec.mx>
2026-04-16 02:40:32 -0600

    hola

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
@@ -30,23 +30,27 @@ public class OracleConfiguration {
     @Bean
     public DataSource dataSource() throws SQLException{
         OracleDataSource ds = new OracleDataSource();
-//        For production/Docker (uses environment variables)
-//        ds.setDriverType(env.getProperty("driver_class_name"));
-//        logger.info("Using Driver " + env.getProperty("driver_class_name"));
-//        ds.setURL(env.getProperty("db_url"));
-//        logger.info("Using URL: " + env.getProperty("db_url"));
-//        ds.setUser(env.getProperty("db_user"));
-//        logger.info("Using Username " + env.getProperty("db_user"));
-//        ds.setPassword(env.getProperty("dbpassword"));
-        // For local testing
-        System.setProperty("oracle.net.tns_admin", dbSettings.getTnsAdmin());
-        ds.setDriverType(dbSettings.getDriver_class_name());
-        logger.info("Using Driver " + dbSettings.getDriver_class_name());
-        ds.setURL(dbSettings.getUrl());
-        logger.info("Using URL: " + dbSettings.getUrl());
-        ds.setUser(dbSettings.getUsername());
-        logger.info("Using Username: " + dbSettings.getUsername());
-        ds.setPassword(dbSettings.getPassword());
+        String dbUrl = env.getProperty("db_url");
+        if (dbUrl != null) {
+            // Production/Kubernetes: use environment variables
+            ds.setDriverType(env.getProperty("driver_class_name"));
+            logger.info("Using Driver " + env.getProperty("driver_class_name"));
+            ds.setURL(dbUrl);
+            logger.info("Using URL: " + dbUrl);
+            ds.setUser(env.getProperty("db_user"));
+            logger.info("Using Username " + env.getProperty("db_user"));
+            ds.setPassword(env.getProperty("dbpassword"));
+        } else {
+            // Local testing: use application.properties via dbSettings
+            System.setProperty("oracle.net.tns_admin", dbSettings.getTnsAdmin());
+            ds.setDriverType(dbSettings.getDriver_class_name());
+            logger.info("Using Driver " + dbSettings.getDriver_class_name());
+            ds.setURL(dbSettings.getUrl());
+            logger.info("Using URL: " + dbSettings.getUrl());
+            ds.setUser(dbSettings.getUsername());
+            logger.info("Using Username: " + dbSettings.getUsername());
+            ds.setPassword(dbSettings.getPassword());
+        }
         return ds;
     }
 }
