9542eed6dc03139abc12eaaa858fce1ec57e4a42
API REST <a01723282@tec.mx>
2026-04-16 00:09:29 -0600

    aksjdka

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.springboot.MyTodoList.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
